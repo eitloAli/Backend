e4ef384b7449ee726c9732981690520165f1f95c
Muhammad Ali <mohammedalikalam1714@gmail.com>
2024-04-19 23:04:59 +0600

    initialize phase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+import  mongoose  from "mongoose";
+import {DB_NAME} from "./constants" 
+import express from 'express'
+const app = express()
+
+;(async () =>  {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log()
+        })
+    } catch (error) {
+        console.error("Error while connecting Database", error)
+        throw error
+    }
+})()
