d9ac1c8605f32c5e16fde25c3fe9d37383a4bc7f
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-05-04 22:32:32 +0600

    refreshAccessToken route has been added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 const router = Router()
@@ -21,7 +21,10 @@ router.route("/register").post(
 )
 
 router.route("/login").post(loginUser)
+router.route("/refresh-access-token").post(refreshAccessToken)
+
 // secure routes
-router.route("/logout").post( verifyJWT, logoutUser )
+router.route("/logout").post(verifyJWT, logoutUser)
+
 
 export default router
