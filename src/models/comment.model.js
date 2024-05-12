55bb3b682c8c5d9485b705211904fbfb309ba082
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-05-12 16:11:35 +0600

    Comment model done

---
+++ src/models/comment.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+const commentSchema = new Schema(
+    {
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User",
+        },
+        comment : {
+            type : String,
+            required : true
+        },
+        video : {
+            type : Schema.Types.ObjectId,
+            ref : "Video"
+        }
+    },
+    {
+        timestamps : true 
+    }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema)
