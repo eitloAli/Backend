1727c91287dd9af956483a894df8b3cb12357569
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-05-12 15:57:03 +0600

    just managed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -37,13 +37,19 @@ router.route("/refresh-access-token").post(refreshAccessToken);
 
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/current-user").post(verifyJWT, getCurrentUser);
-router.route("/update-user-details").post(verifyJWT, updateAccountDetails);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
 router.route("/update-current-password").post(verifyJWT, updateCurrentPassword);
-router.route("/test").get(cloud)
-router.route("/update-avatar-image").post(verifyJWT, upload.single("avatar"), updateAvatarImage)
-router.route("/update-cover-image").post(verifyJWT, upload.single("cover"), updateCoverImage)
-router.route("/c/:username").post(verifyJWT, getUserChanneDetails)
-router.route("/subscribe").post(verifyJWT, subscribe)
+
+router.route("/update-avatar-image").patch(verifyJWT, upload.single("avatar"), updateAvatarImage)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("cover"), updateCoverImage)
+
+
+router.route("/c/:username").get(verifyJWT, getUserChanneDetails)
 router.route("/user-watch-history").post(verifyJWT, getWatchedHistory)
+
+
+router.route("/test").get(cloud)
+router.route("/subscribe").post(verifyJWT, subscribe)
+
 export default router;
