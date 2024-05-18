b3e254ed7204a4c3c4f4ddb85f175c0322320232
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-05-18 19:57:54 +0600

    successfully write the functionality of video controller except for updateV and getAllV, also not tested yet

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -10,6 +10,7 @@ import {
 
 import fs from "fs";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { json } from "express";
 
 const getAllvideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
@@ -60,29 +61,75 @@ const publishAVideo = asyncHandler(async (req, res) => {
         );
 });
 
-const getVideoById = asyncHandler(async (req,res) => {
-    const {videoId} = req.params;
-})
+const getVideoById = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const validObjectId = isValidObjectId(videoId);
 
-const updateVideo = asyncHandler(async (req,res) => {
-    const {videoId} = req.params;
-    const {title, description} = req.body
-})
+    if (!validObjectId || videoId) {
+        throw new ApiError(404, "Video not found");
+    }
 
-const deleteVideo = asyncHandler(async (req,res) => {
-    const {videoId} = req.params;
-})
+    const video = await Video.aggregate([
+        {
+            $match: new mongoose.Types.ObjectId(videoId),
+        },
+    ]);
 
-const togglePublishStatus = asyncHandler(async (req,res) => {
-    const {videoId} = req.params;
-})
+    if (!video) {
+        throw new ApiError(404, "No video Found in database");
+    }
 
+    res.status(200).json(
+        new ApiResponse(200, video, "Video successfully fetched")
+    );
+});
 
-export { 
+const updateVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const { title, description } = req.body;
+});
+
+const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+
+    const existedVideo = await Video.findById(videoId);
+
+    if (!existedVideo) {
+        throw new ApiError(404, "No video found for deleting");
+    }
+
+    if (existedVideo?._id !== req?.user._id) {
+        throw new ApiError(400, "you are not authorize to delete this video");
+    }
+
+    const videoDeleted = await Video.deleteOne(videoId);
+
+    res.status(200).json(200, videoDeleted, "successfully deleted the video");
+});
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "No video found for changing the Publish status")
+    }
+
+    video.isPublished = !video.isPublished
+    
+    const statusUpdated = await video.save()
+    
+    return res
+        .status(200)
+        .json(new ApiResponse(200, statusUpdated, "Publish status has been updated"))
+});
+
+export {
     getAllvideos,
     publishAVideo,
     getVideoById,
     updateVideo,
     deleteVideo,
-    togglePublishStatus
+    togglePublishStatus,
 };
