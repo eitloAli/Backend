9cae6a9dc5b58e3a3ca7196e206d7db02ddb6d20
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-05-18 19:22:52 +0600

    intialized and set the all routes for video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,69 +1,88 @@
-import mongoose,{isValidObjectId} from "mongoose"
-import { Video } from "../models/video.model.js"
-import { User } from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import { 
-uploadOnCloudinary,
-deleteCloudinaryImage } from "../utils/cloudinary.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import {
+    uploadOnCloudinary,
+    deleteCloudinaryImage,
+} from "../utils/cloudinary.js";
 
-import fs from "fs"
-import { asyncHandler } from "../utils/asyncHandler.js"
+import fs from "fs";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const getAllvideos = asyncHandler( async (req,res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-})
+const getAllvideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+});
 
-const publishVideo = asyncHandler( async (req,res) => {
-    const {title, description} =  req.body
+const publishAVideo = asyncHandler(async (req, res) => {
+    const { title, description } = req.body;
     if (!title) {
-        fs.unlinkSync(req?.files?.video[0]?.path ?? "")
-        fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "")
-        throw new ApiError(400, "Title is required")
+        fs.unlinkSync(req?.files?.videoFile[0]?.path ?? "");
+        fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "");
+        throw new ApiError(400, "Title is required");
     }
 
-    if(req?.files?.video){
-        fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "")
-        throw new ApiError(400, "Video is required")
+    if (!req?.files?.videoFile) {
+        fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "");
+        throw new ApiError(400, "Video is required");
     }
 
-    const validOwner = isValidObjectId(req?.user._id)
+    const validOwner = isValidObjectId(req?.user._id);
 
     if (!validOwner) {
-        throw new ApiError(400, "you are not authorized to upload this video")
+        throw new ApiError(400, "you are not authorized to upload this video");
     }
 
-    const uploadedVideo = await uploadOnCloudinary(req.files.video[0].path)
-    const uploadedThumbnail = await uploadOnCloudinary(req.files.video[0].path)
-    
-    fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "")
-    fs.unlinkSync(req?.files?.video[0]?.path ?? "")
-    
-    console.log(video);
-    
-    const video = await Video.create(
-        {
-            videoFile : uploadedVideo?.url || "URL not found",
-            thumbnail : uploadedThumbnail?.url || "URL not found",
-            title,
-            description,
-            duration : uploadedVideo?.duration || 404,
-            owner : req.user._id
+    const uploadedVideo = await uploadOnCloudinary(req.files.videoFile[0].path);
+    const uploadedThumbnail = await uploadOnCloudinary(
+        req.files.thumbnail[0].path
+    );
 
-        }
-    )
+    fs.unlinkSync(req?.files?.videoFile[0]?.path ?? "");
+    fs.unlinkSync(req?.files?.thumbnail[0]?.path ?? "");
+
+    const video = await Video.create({
+        videoFile: uploadedVideo?.url || "URL not found",
+        thumbnail: uploadedThumbnail?.url || "URL not found",
+        title,
+        description,
+        duration: uploadedVideo?.duration || 404,
+        owner: req.user._id,
+    });
+
+    console.log(video);
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            video,
-            "video has been published successfully"
-        )
-    )
+        .status(200)
+        .json(
+            new ApiResponse(200, video, "video has been published successfully")
+        );
+});
+
+const getVideoById = asyncHandler(async (req,res) => {
+    const {videoId} = req.params;
 })
-export {
-    publishVideo
-}
- 
+
+const updateVideo = asyncHandler(async (req,res) => {
+    const {videoId} = req.params;
+    const {title, description} = req.body
+})
+
+const deleteVideo = asyncHandler(async (req,res) => {
+    const {videoId} = req.params;
+})
+
+const togglePublishStatus = asyncHandler(async (req,res) => {
+    const {videoId} = req.params;
+})
+
+
+export { 
+    getAllvideos,
+    publishAVideo,
+    getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus
+};
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,4 +1,32 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { deleteVideo, getAllvideos, publishAVideo, updateVideo,  } from "../controllers/video.controller.js"
 const videoRouter = Router()
+videoRouter.use(verifyJWT)
+
+videoRouter.route("/")
+    .get()
+    .post(
+        upload.fields(
+            [
+                {
+                    name : "videoFile",
+                    maxCount : 1
+                },
+                {
+                    name : "thumbnail",
+                    maxCount : 1
+                }
+            ]
+        ),
+        publishAVideo
+    );
+
+videoRouter
+    .route("/:videoId")
+    .get( getAllvideos )
+    .delete( deleteVideo )
+    .patch(upload.single("thumbnail"), updateVideo)
 
 export default videoRouter
