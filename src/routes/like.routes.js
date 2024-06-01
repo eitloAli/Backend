a420b224174102bdcea159ffcba3d2eef3678921
Mohammed Ali <153717237+eitloAli@users.noreply.github.com>
2024-06-01 23:55:46 +0600

    Update like.routes.js

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,4 +1,19 @@
 import { Router } from "express";
 const likeRouter = Router()
+import {
+    getLikedVideos,
+    toggleCommentLike,
+    toggleVideoLike,
+    toggleTweetLike,
+} from "../controllers/like.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
-export default likeRouter
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/toggle/v/:videoId").post(toggleVideoLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
+
+export default router
