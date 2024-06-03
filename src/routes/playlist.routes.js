7779e11c10dba8a1df53954dd16ab6ca75c4cdd9
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-06-03 18:11:01 +0600

    bew routes added for likes and playlist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,32 +1,32 @@
-import { Router } from "express";
-const playlistRouter = Router()
 
-export default playlistRouter
+
 import { Router } from 'express';
-import {
-    addVideoToPlaylist,
-    createPlaylist,
-    deletePlaylist,
-    getPlaylistById,
-    getUserPlaylists,
-    removeVideoFromPlaylist,
-    updatePlaylist,
-} from "../controllers/playlist.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+// import {
+//     addVideoToPlaylist,
+//     createPlaylist,
+//     deletePlaylist,
+//     getPlaylistById,
+//     getUserPlaylists,
+//     removeVideoFromPlaylist,
+//     updatePlaylist,
+// } from "../controllers/playlist.controller.js"
+// import {verifyJWT} from "../middlewares/auth.middleware.js"
 
-playlistRouter.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-playlistRouter.route("/").post(createPlaylist)
+const playlistRouter = Router()
+// playlistRouter.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-playlistRouter
-    .route("/:playlistId")
-    .get(getPlaylistById)
-    .patch(updatePlaylist)
-    .delete(deletePlaylist);
+// playlistRouter.route("/").post(createPlaylist)
 
-playlistRouter.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
-playlistRouter.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+// playlistRouter
+//     .route("/:playlistId")
+//     .get(getPlaylistById)
+//     .patch(updatePlaylist)
+//     .delete(deletePlaylist);
 
-playlistRouter.route("/user/:userId").get(getUserPlaylists);
+// playlistRouter.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
+// playlistRouter.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+
+// playlistRouter.route("/user/:userId").get(getUserPlaylists);
 
 export default playlistRouter
