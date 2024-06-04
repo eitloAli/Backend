e1d2445f66dd7b1b48a158d4756056a7025c73ad
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-06-04 19:03:55 +0600

    just modification with subscription routes

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,4 +1,21 @@
-import { Router } from "express";
-const subscriptionRouter = Router()
+import { Router } from 'express';
+import {
+    // getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription,
+    test,
+    getSubscribedChannel
+} from "../controllers/subscription.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
-export default subscriptionRouter
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/c/:channelId")
+    .get(getSubscribedChannel)
+    .post(toggleSubscription)
+
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+
+export default router
