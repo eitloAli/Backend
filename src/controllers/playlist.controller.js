f56164cddbe942bb0f22d9a072fa0809428ff143
Mohammed Ali <mohammedalikalam1714@gmail.com>
2024-06-07 21:21:28 +0600

    playlist route and controller has done but some of route is remain for testing

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -125,51 +125,139 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const { playlistId, videoId } = req.params;
 
     if (!isValidObjectId(playlistId) || !isValidObjectId(video)) {
-        throw new ApiError(400, "Playlist id or Video id is not valid")
+        throw new ApiError(400, "Playlist id or Video id is not valid");
     }
 
-    const video_id = new mongoose.Types.ObjectId(videoId)
+    const video_id = new mongoose.Types.ObjectId(videoId);
 
     const video = await Video.findById(videoId);
-    
+
     if (!video) {
-        throw new ApiError(404, "Video not found for adding in playlist")
+        throw new ApiError(404, "Video not found for adding in playlist");
+    } else if (!video.isPublished) {
+        throw new ApiError(400, "video is unpublished or private");
     }
 
+    const existingPlaylist = await Playlist.findById(playlistId);
 
-    const playlist = await Playlist.findByIdAndUpdate(playlistId)
-
-    if (!playlist) {
-        throw new ApiError(400, "Playlist not found please enter the valid playlist id")
+    if (!existingPlaylist) {
+        throw new ApiError(
+            400,
+            "Playlist not found please enter the valid playlist id"
+        );
+    } else if (existingPlaylist?._id !== req.user._id) {
+        throw new ApiError(
+            400,
+            "you are not authorized to update this playlist"
+        );
+    } else if (existingPlaylist.videos?.includes(video_id)) {
+        throw new ApiError(400, "Video is already in Playlist");
     }
 
+    await existingPlaylist.videos.push(video_id);
 
-    if (playlist?._id !== req.user._id) {
-        throw new ApiError(400, "you are not authorized to update this playlist")   
-    }
+    const playlistUpdated = await existingPlaylist.save({
+        validateModifiedOnly: true,
+    });
 
-
-    if (playlist.videos?.includes(video_id)) {
-        throw new ApiError(400, "Video is already in Playlist")
-    }
-
-    playlist.videos.push(video_id)
-
-    const playlistUpdated = await playlist.save({validateBeforeSave : true})
-    
-    
     return res
-            .status(200)
-            .json(new ApiResponse(
+        .status(200)
+        .json(
+            new ApiResponse(
                 200,
                 playlistUpdated,
                 "Video has been successfully added in playlist"
-            ))
+            )
+        );
 });
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Playlist id or Object id is not valid");
+    }
+    const existingPlaylist = await Playlist.findById(playlistId);
+
+    if (!existingPlaylist) {
+        throw new ApiError(404, "Playlist not found");
+    } else if (existingPlaylist?.owner !== req?.user?._id) {
+        throw new ApiError(
+            400,
+            "you are not autorized to update this playlist"
+        );
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found for adding to playlist");
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull: {
+                vidoes: video._id || " ",
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                playlistId,
+                "Video has been removed from the playlist"
+            )
+        );
+});
+
+const getUserPlaylists = asyncHandler(async (req,res) => {
+    const {userId} = req.params
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "object id is not valid")
+    }
+    else if (userId !== req?.user?._id.toString()) {
+        throw new ApiError(400, "you are not autorize to access this playlist")
+    }
+
+    const playlists = await Playlist.aggregate(
+        [
+            {
+                $match : {
+                    owner : req?.user?._id
+                }
+            },
+            {
+                $lookup : {
+                    from : "Video",
+                    localField : "videos",
+                    foreignField : "_id",
+                    as : "video"
+                }
+            }
+        ]
+    );
+
+    return res 
+            .status(200)
+            .json(new ApiResponse(
+                200,
+                playlists,
+                `Playlist fetched successfully for this user ${req?.username}`
+            ))
+})
 export {
     createPlaylist,
     getPlaylistById,
     updatePlaylist,
     deletePlaylist,
     addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    getUserPlaylists,
 };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,32 +1,31 @@
-
-
-import { Router } from 'express';
+import { Router } from "express";
 import {
     createPlaylist,
     getPlaylistById,
     updatePlaylist,
     deletePlaylist,
     addVideoToPlaylist,
-    // getUserPlaylists,
-    // removeVideoFromPlaylist,
-} from "../controllers/playlist.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+    removeVideoFromPlaylist,
+    getUserPlaylists,
+} from "../controllers/playlist.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
-const playlistRouter = Router()
+const playlistRouter = Router();
 playlistRouter.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-playlistRouter.route("/").post(createPlaylist)
+playlistRouter.route("/").post(createPlaylist);
 
 playlistRouter
     .route("/:playlistId")
-        .get(getPlaylistById)
-        .patch(updatePlaylist)
-        .delete(deletePlaylist);
+    .get(getPlaylistById)
+    .patch(updatePlaylist)
+    .delete(deletePlaylist);
 
 playlistRouter.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
-// playlistRouter.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
+playlistRouter
+    .route("/remove/:videoId/:playlistId")
+    .patch(removeVideoFromPlaylist);
 
-// playlistRouter.route("/user/:userId").get(getUserPlaylists);
+playlistRouter.route("/user/:userId").get(getUserPlaylists);
 
-export default playlistRouter
+export default playlistRouter;
